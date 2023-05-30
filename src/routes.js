f5f4d8da44e83d36f23dc26f2f4b02e8b41d4fe8
CamposLeo95 <leocampos.995@gmail.com>
2023-05-30 15:12:56 -0300

    build(eslint prettier): config eslint and prettier

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>{
-    return res.json({message: "Hello App"});
-})
+routes.get("/", (req, res) => res.json({ message: "Hello App" }));
 
-module.exports = routes;
+export default routes;
