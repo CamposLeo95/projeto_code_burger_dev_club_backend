0a313cf2cbb3dbb07b5e39244fc5ec0abb779e4c
CamposLeo95 <leocampos.995@gmail.com>
2023-05-31 15:31:02 -0300

    feat(connectmodels): connecting database with models
    ]

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
+import { v4 } from "uuid";
+
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => res.json({ message: "Hello App" }));
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    id: v4(),
+    name: "Leonardo",
+    email: "Leo@email.com",
+    password_hash: "2dsa65d1d",
+  });
+
+  return res.json(user);
+});
 
 export default routes;
