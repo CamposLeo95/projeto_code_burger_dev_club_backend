14304704f9856ec0da9a38d40370e0f2d2c52d3c
CamposLeo95 <leocampos.995@gmail.com>
2023-06-12 15:35:57 -0300

    feat(path category): create column path on database and update on categorycontroller

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,5 +1,6 @@
 import * as Yup from "yup";
 import Category from "../models/Category";
+import User from "../models/User";
 
 class CategoryController {
   async store(req, res) {
@@ -13,13 +14,22 @@ class CategoryController {
       return res.status(400).json({ msg: err.errors });
     }
 
+    const { admin: isAdmin } = await User.findByPk(req.userId);
+
+    if (!isAdmin) {
+      return res.status(401).json();
+    }
+
     const { name } = req.body;
+
+    const { filename: path } = req.file;
+
     const category = await Category.findOne({ where: { name } });
     if (category) {
       return res.status(400).json({ message: "Category already exists" });
     }
 
-    const { id } = await Category.create({ name });
+    const { id } = await Category.create({ name, path });
 
     return res.status(400).json({ message: { id, name } });
   }
@@ -29,6 +39,45 @@ class CategoryController {
 
     return res.status(400).json(categories);
   }
-}
 
+  async update(req, res) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+      });
+
+      try {
+        await schema.validateSync(req.body);
+      } catch (err) {
+        return res.status(400).json({ msg: err.errors });
+      }
+
+      const { admin: isAdmin } = await User.findByPk(req.userId);
+
+      if (!isAdmin) {
+        return res.status(401).json();
+      }
+
+      const { name } = req.body;
+
+      const { id } = req.params;
+
+      const categoryId = await Category.findByPk(id);
+
+      if (!categoryId) {
+        return res.status(400).json({ error: "Make sure your id is correct" });
+      }
+      let path;
+      if (req.file) {
+        path = req.file.filename;
+      }
+
+      await Category.update({ name, path }, { where: { id } });
+
+      return res.status(200).json();
+    } catch (error) {
+      return res.status(400).json();
+    }
+  }
+}
 export default new CategoryController();
